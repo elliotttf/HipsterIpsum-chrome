0a4b2135947dfaa3bdca81125a15d564fd872da4
Elliott Foster <elliottf@codebrews.com>
2012-01-02 10:48:14 -0600

    Removed dpenency on API.

--- ipsum.js
+++ ipsum.js
@@ -1,7 +1,14 @@
 /**
- * Ipsum generator.
+ * @fileoverview Ipsum generator class.
  */
 
+/**
+ * @class
+ * @param {int} para
+ *   Number of paragraphs to generate.
+ * @param {boolean} html
+ *   Flag to include HTML tags or not.
+ */
 function Ipsum(para, html) {
   if (typeof para === 'undefined') {
     this.para = 4;
@@ -18,22 +25,59 @@ function Ipsum(para, html) {
   }
 }
 
+/**
+ * @const
+ */
 Ipsum.latin = [
   'a','adipiscing','amet','arcu','at','auctor','bibendum','commodo','congue','curabitur','cursus','diam','donec','duis','eget','elementum','enim','eros','et','eu','fusce','gravida','in','integer','ipsum','justo','lectus','leo','ligula','lorem','maecenas','magna','malesuada','massa','mattis','mauris','metus','molestie','morbi','nam','nec','nibh','non','nulla','odio','orci','ornare','pellentesque','pharetra','porta','porttitor','proin','quam','quisque','risus','rutrum','sagittis','sapien','sed','sem','sit','sodales','tellus','tempus','ultricies','urna','ut','vitae','vivamus','vulputate'
 ];
 
+/**
+ * @const
+ */
 Ipsum.hipster = [
+  '+1',
+  '8-bit',
+  'artisan',
+  'bahn mi',
+  'before they sold out',
+  'bicycle',
+  'biodiesel',
+  'brunch',
+  'craft beer',
+  'cred',
   'farm-to-table',
-  'VHS',
+  'fixie',
+  'food truck',
+  'hoodie',
+  'indie',
+  'keytar',
+  'mustache',
+  'organic',
+  'specs',
+  'skateboard',
+  'tofu',
+  'vegan',
+  'vinyl',
+  'viral',
+  "you probably haven't heard of them",
   'Austin',
   'Brooklyn',
-  'before they sold out',
-  "you probably haven't heard of them",
+  'DIY',
+  'PBR',
+  'Portland',
+  'San Francisco',
+  "Tom's",
+  'VHS'
 ];
 
+/**
+ * Returns a HipsterIpsum string.
+ */
 Ipsum.prototype.get = function() {
   var ret = '';
   var wCount = 0;
+  var hipDist = Math.round(Math.random() * (10 - 3) + 3);
 
   // Create the correct number of paragraphs.
   for (var p = 0; p < this.para; p++) {
@@ -48,14 +92,22 @@ Ipsum.prototype.get = function() {
       var sLen = Math.round(Math.random() * (20 - 15) + 15);
       for (var w = 0; w < sLen; w++, wCount++) {
         var i = 0;
-        if (wCount % 5 === 0) {
+        var word = '';
+        if (wCount % hipDist === 0) {
           i = Math.round(Math.random() * Ipsum.hipster.length);
-          ret += Ipsum.hipster[i];
+          word = Ipsum.hipster[i];
         }
         else {
           i = Math.round(Math.random() * Ipsum.latin.length);
-          ret += Ipsum.latin[i];
+          word = Ipsum.latin[i];
         }
+
+        // Capitalize the first word in the sentence.
+        if (w === 0) {
+          word = word.charAt(0).toUpperCase() + word.slice(1);
+        }
+
+        ret += word;
 
         if (w + 1 < sLen) {
           ret += ' ';
@@ -68,11 +120,13 @@ Ipsum.prototype.get = function() {
       }
     }
 
-    if (this.html) {
-      ret += "</p>\n";
-    }
-    else {
-      ret += "\n\n";
+    if (p + 1 < this.para) {
+      if (this.html) {
+        ret += "</p>\n";
+      }
+      else {
+        ret += "\n\n";
+      }
     }
   }
 
