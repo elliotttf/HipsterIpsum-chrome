43c5b77b75e9819b9bd01daee3f6e0225c73ff42
Elliott Foster <elliottf@codebrews.com>
2012-09-11 12:56:33 -0500

    Fixing banner generation.

--- grunt.js
+++ grunt.js
@@ -11,8 +11,8 @@ module.exports = function(grunt) {
       banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>);' +
+        ' Licensed <%= pkg.license %> */'
     },
     lint: {
       files: ['ipsum.js', 'popup.js']
